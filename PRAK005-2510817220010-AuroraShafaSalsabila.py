278c4d861d10f88810599837595dfd080f2a19ba
avrshaf <aurorashafasalsabilaa07@gmail.com>
2025-09-23 22:01:52 +0800

    Update PRAK005-2510817220010-AuroraShafaSalsabila.py

--- PRAK005-2510817220010-AuroraShafaSalsabila.py
+++ PRAK005-2510817220010-AuroraShafaSalsabila.py
@@ -1,9 +1,5 @@
-def print_A():
-    print("  #     #  ")
-    print(" # #   # # ")
-    print("##### #####")
-    print("#   # #   #")
-    print("#   # #   #")
-
-if __name__ == "__main__":
-    print_A()
+print("     #         #    ")
+print("    # #       # #   ")
+print("   #   #     #   #  ")
+print("  #######   ####### ")
+print(" #       # #       #")
