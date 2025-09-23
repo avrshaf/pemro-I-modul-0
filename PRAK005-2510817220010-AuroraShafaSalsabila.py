69f7dd03c6ef7fa7272ad4234b9b8d80eb450cef
A771025 <aurorashafasalsabilaa07@gmail.com>
2025-09-23 20:29:44 +0800

    Add files via upload

---
+++ PRAK005-2510817220010-AuroraShafaSalsabila.py
@@ -0,0 +1,9 @@
+def print_A():
+    print("  #     #  ")
+    print(" # #   # # ")
+    print("##### #####")
+    print("#   # #   #")
+    print("#   # #   #")
+
+if __name__ == "__main__":
+    print_A()
