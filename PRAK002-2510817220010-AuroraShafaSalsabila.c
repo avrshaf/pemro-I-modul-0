cff669058625e72d34fb133027b23fd899a8f40c
avrshaf <aurorashafasalsabilaa07@gmail.com>
2025-09-24 21:00:59 +0800

    Update PRAK002-2510817220010-AuroraShafaSalsabila.c

--- PRAK002-2510817220010-AuroraShafaSalsabila.c
+++ PRAK002-2510817220010-AuroraShafaSalsabila.c
@@ -3,6 +3,6 @@
 int main(){
     printf("Selamat Pagi Aurora Shafa Salsabila\n");
     printf("Selamat Siang Aurora Shafa Salsabila\n");
-    printf("Selamat Sore Aurora Shafa Salsabila\n");
+    printf("Selamat Malam Aurora Shafa Salsabila\n");
 return 0;
 }
