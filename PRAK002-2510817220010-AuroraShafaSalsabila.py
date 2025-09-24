a651ffd3cada2be35a2d45f808416369b77f2517
avrshaf <aurorashafasalsabilaa07@gmail.com>
2025-09-24 21:01:47 +0800

    Update PRAK002-2510817220010-AuroraShafaSalsabila.py

--- PRAK002-2510817220010-AuroraShafaSalsabila.py
+++ PRAK002-2510817220010-AuroraShafaSalsabila.py
@@ -1,3 +1,3 @@
 print("Selamat Pagi Aurora Shafa Salsabila")
-print("Selamat Sore Aurora Shafa Salsabila")
+print("Selamat Siang Aurora Shafa Salsabila")
 print("Selamat Malam Aurora Shafa Salsabila")
